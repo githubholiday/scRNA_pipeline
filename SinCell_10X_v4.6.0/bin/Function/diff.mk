650cd9aff643b8a703570cfeed22d07b336ed5ec
chengfangtu <1161671154@qq.com>
2023-11-28 16:47:22 +0800

    流程推送

--- SinCell_10X_v4.6.0/bin/Function/diff.mk
+++ SinCell_10X_v4.6.0/bin/Function/diff.mk
@@ -29,7 +29,7 @@ Annotation_diff:
 		$(PYTHON3) $(tmpdir)/add_annotation.py -i $$dir/$$name\_symbol.xls -ar $(REF_ANNOTATION) -g $(GTF) -out $$dir/$$name\_symbol.anno.xls -c 0 0 ;done
 	$(PYTHON3) $(BIN)/Function/anno/DEnumber_stat_summary.py -d $(de_file) -s $(compare) -o $(de_dir)/$(compare).de_stat.xls
 	echo Annotation for DIFF end	
-	
+
 GO:
 	echo "#############GO analsyis start at "`date`
 	[ -d $(go_shell_dir) ] || mkdir -p $(go_shell_dir)
@@ -41,7 +41,7 @@ GO:
 KEGG:
 	echo "#############KEGG analsyis start at "`date`
 	[ -d $(kegg_shell_dir) ] || mkdir -p $(kegg_shell_dir)
-	$(PYTHON3) $(BIN)/Function/generate_shell.py -i $(de_file) -o $(kegg_shell) -a kegg -s $(species) -c $(compare) -ca $(category) --sif $(sif)
+	$(PYTHON3) $(BIN)/Function/generate_shell.py -i $(de_file) -o $(kegg_shell) -a kegg -s $(species) -ca $(category) --sif $(sif)
 	echo '$(SBATCH) -J GO -D $(kegg_shell_dir) -o $(kegg_shell_dir)/go.log -e $(kegg_shell_dir)/go.err -p $(queue) -n 6 --mem 20G --wrap "$(env) $(kegg_shell) && echo GO anaysis finished " > $(kegg_shell_dir)/kegg_qsub.sh
 	$(QSUB_SGE) --queue sci.q --lines 1 --maxjob $(maxjob) --resource "vf=20G -l p=2" --maxcycle 1 --quota 1000G --jobidstart 0 $(kegg_shell) && echo KEGG analysis finished
 	echo "#############KEGG analysis end at" `date`
