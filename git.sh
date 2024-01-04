5fd792e9c52170709ba61054d672784bc72e031c
chengfangtu <1161671154@qq.com>
2024-01-04 15:51:48 +0800

    流程推送

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 git add -A 
 git commit -m "流程推送"
-#git push
-git tag v1.0.0 -m "第一版本流程推送，本次已测试通过"
-git push origin v1.0.0
+git push
+#git tag v1.0.0 -m "第一版本流程推送，本次已测试通过"
+#git push origin v1.0.0
 #git push origin dev:dev
